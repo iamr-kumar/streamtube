c141500881cc24f2ebd14da819a78fd2466af7af
Ritik Kumar <ritik.kumar006@gmail.com>
2024-09-21 21:15:32 +0530

    exclude streamer from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, {}) => {
+    // Exclude the streamer folder from being processed by Next.js
+    config.module.rules.push({
+      test: /\.(ts|tsx|js|jsx)$/,
+      exclude: [/node_modules/, /src\/streamer/],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
