6a5cc4b85b7685a5e5cbf429d4456913420c0cc7
Ritik Kumar <ritik.kumar006@gmail.com>
2024-09-29 03:42:54 +0530

    basic streaming working

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
 export default function Studio() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const searchParams = useSearchParams();
   const rtmpUrl = searchParams.get("rtmpUrl");
-  console.log(rtmpUrl);
+  console.log("rtmpUrl: ", rtmpUrl);
+  const [userStream, setUserStream] = useState<MediaStream | null>(null);
+  const [socketIo, setSocketIo] = useState<ReturnType<typeof io> | null>(null);
 
   useEffect(() => {
     async function getVideo() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
           video: true,
         });
+        setUserStream(stream);
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
@@ -25,27 +29,48 @@ export default function Studio() {
 
     getVideo();
 
-    const socketIo = io("http://localhost:3001");
+    const socket = io("http://localhost:3001");
+    setSocketIo(socket);
 
-    // Set up socket event listeners
-    socketIo.on("connect", () => {
+    socket.on("connect", () => {
       console.log("Connected to WebSocket");
     });
 
-    socketIo.on("disconnect", () => {
+    socket.on("disconnect", () => {
       console.log("Disconnected from WebSocket");
     });
 
-    // Clean up connection on component unmount
     return () => {
-      socketIo.disconnect();
+      socket.disconnect();
     };
   }, []);
+
+  const onStart = async () => {
+    if (!userStream || !socketIo) return;
+    const mediaRecorder = new MediaRecorder(userStream, {
+      audioBitsPerSecond: 128000,
+      videoBitsPerSecond: 2500000,
+      mimeType: "video/webm",
+    });
+
+    console.log("mediaRecorder: ", mediaRecorder);
+
+    mediaRecorder.ondataavailable = (event) => {
+      socketIo.emit("videoData", event.data);
+    };
+
+    mediaRecorder.start(25);
+  };
 
   return (
     <div className="p-12">
       <video ref={videoRef} autoPlay></video>
-      <button className="mt-4 p-2 bg-blue-500 text-white rounded">Start</button>
+      <button
+        className="mt-4 p-2 bg-blue-500 text-white rounded"
+        onClick={onStart}
+      >
+        Start
+      </button>
     </div>
   );
 }
