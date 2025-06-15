cf70be4f36a9c6810651e81f25e2e8381926d632
Ritik Kumar <ritik.kumar006@gmail.com>
2025-06-16 04:04:26 +0530

    feat(auth): integrate NextAuth with Google provider and JWT session strategy
    - Implemented NextAuth for authentication in the application.
    - Configured Google as an authentication provider with necessary scopes.
    - Added JWT session strategy to manage user sessions.
    - Created custom session and JWT types to include accessToken.
    - Added SessionProvider component to wrap the application with NextAuth session context.

---
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+
+const authOption: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
+
+const handler = NextAuth(authOption);
+export { handler as GET, handler as POST };
